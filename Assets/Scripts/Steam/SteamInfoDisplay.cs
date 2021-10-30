129958aeb20a948ba5ddf5fb862dd9d71ed0aa13
Ramin Amiri <zeuspsn.55555@gmail.com>
2021-10-30 14:47:56 -0400

    steamworks stuff
    steam name and avatar

---
+++ Assets/Scripts/Steam/SteamInfoDisplay.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Steamworks;
+
+public class SteamInfoDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI playerNameText;
+    [SerializeField] private RawImage playerAvatar;
+
+    void Start()
+    {
+        playerNameText.SetText(SteamFriends.GetPersonaName());
+
+        int imageID = SteamFriends.GetLargeFriendAvatar(SteamUser.GetSteamID());
+        playerAvatar.texture = GetSteamImageAsTexture2D(imageID);
+    }
+
+    public static Texture2D GetSteamImageAsTexture2D(int iImage) {
+    Texture2D ret = null;
+    uint ImageWidth;
+    uint ImageHeight;
+    bool bIsValid = SteamUtils.GetImageSize(iImage, out ImageWidth, out ImageHeight);
+
+    if (bIsValid) {
+        byte[] Image = new byte[ImageWidth * ImageHeight * 4];
+
+        bIsValid = SteamUtils.GetImageRGBA(iImage, Image, (int)(ImageWidth * ImageHeight * 4));
+        if (bIsValid) {
+            ret = new Texture2D((int)ImageWidth, (int)ImageHeight, TextureFormat.RGBA32, false, true);
+            ret.LoadRawTextureData(Image);
+            ret.Apply();
+        }
+    }
+
+    return ret;
+}
+}
